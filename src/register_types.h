a301cb130b613dd5b92929f205e8ac80101259b5
Jarik R <jarik.ruedebusch@web.de>
2023-09-13 14:18:01 +0200

    Add files to register class in Godot

---
+++ src/register_types.h
@@ -0,0 +1,7 @@
+#ifndef DATAGRID_REGISTER_TYPES_H
+#define DATAGRID_REGISTER_TYPES_H
+
+void initialize_data_grid_module();
+void uninitialize_data_grid_module();
+
+#endif // DATAGRID_REGISTER_TYPES_H
