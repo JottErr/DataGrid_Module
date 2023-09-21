c72d6b811d4c88effec04d1c8d29aba78c09bc0a
Jarik R <jarik.ruedebusch@web.de>
2023-09-21 17:03:32 +0200

    Add DataGridCollection Class
    Introduces DataGridCollection class. It is used to organize DataGrids in
    a DataGridManager. DataGridCollection has a Dictionary called
    layered_datagrid_collection which maps Vector2i positions to inner
    Dictionaries. The inner Dictionaries map an integer (layer) to a
    DataGrid.
    The outer Dictionary allows to store the positions of multiple Datagrids
    spread over the area covered by the DataManager. The inner Dictionary
    allows to store multiple DataGrids stacked at each postion.
    Using a Dictionary helps to avoid storing empty grids in unpopulated
    areas.
    DataGridCollection also has methods to add, check for, and get DataGrids
    at a specified position and layer. Additionally it can filter layers at
    at a postion to return those DataGrids with the requested layers that
    are present.

---
+++ src/data_grid_collection.cpp
@@ -0,0 +1,65 @@
+#include "data_grid_collection.h"
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void DataGridCollection::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("clear"), &DataGridCollection::clear);
+	ClassDB::bind_method(D_METHOD("add_datagrid_to_collection", "datagrid_position", "layer", "datagrid"), &DataGridCollection::add_datagrid_to_collection);
+	ClassDB::bind_method(D_METHOD("has_layer_at_pos", "datagrid_position", "layer"), &DataGridCollection::has_layer_at_pos);
+	ClassDB::bind_method(D_METHOD("get_layer_at_pos", "datagrid_position", "layer"), &DataGridCollection::get_layer_at_pos);
+	ClassDB::bind_method(D_METHOD("filter_layers_at_pos", "datagrid_position", "filter_layers"), &DataGridCollection::filter_layers_at_pos);
+}
+
+DataGridCollection::DataGridCollection() {
+}
+
+DataGridCollection::~DataGridCollection() {
+}
+
+void DataGridCollection::add_datagrid_to_collection(const Point2i &p_datagrid_position, int p_layer, Ref<DataGrid> p_datagrid) {
+	if (!layered_datagrid_collection.has(p_datagrid_position)) {
+		Dictionary layer_stack;
+		layer_stack[p_layer] = p_datagrid;
+		layered_datagrid_collection[p_datagrid_position] = layer_stack;
+		return;
+	}
+	Dictionary layer_stack = layered_datagrid_collection[p_datagrid_position];
+	if (!layer_stack.has(p_layer)) {
+		layer_stack[p_layer] = p_datagrid;
+	}
+}
+
+Dictionary DataGridCollection::filter_layers_at_pos(const Point2i &p_datagrid_position, const Array &filter_layers) const {
+	Dictionary result; // return an TypedArray[DataGrid].resize[max_layers?20], fill with null
+	if (layered_datagrid_collection.has(p_datagrid_position)) {
+		Dictionary all_layers = layered_datagrid_collection[p_datagrid_position];
+		if (filter_layers.is_empty()) {
+			return all_layers;
+		}
+		for (int i = 0; i < filter_layers.size(); i++) {
+			int layer = filter_layers[i];
+			if (all_layers.has(layer)) {
+				result[layer] = all_layers[layer];
+			}
+		}
+	}
+	return result;
+}
+
+bool DataGridCollection::has_layer_at_pos(const Point2i &p_datagrid_position, int p_layer) const {
+	if (!layered_datagrid_collection.has(p_datagrid_position)) {
+		return false;
+	}
+	Dictionary layer_stack = layered_datagrid_collection[p_datagrid_position];
+	return layer_stack.has(p_layer);
+}
+
+Ref<DataGrid> DataGridCollection::get_layer_at_pos(const Point2i &p_datagrid_position, int p_layer) const {
+	if (!layered_datagrid_collection.has(p_datagrid_position)) {
+		return nullptr;
+	}
+	Dictionary layer_stack = layered_datagrid_collection[p_datagrid_position];
+	Ref<DataGrid> layer = layer_stack[p_layer];
+	return layer;
+}
---
+++ src/data_grid_collection.h
@@ -0,0 +1,30 @@
+#ifndef DATAGRIDCOLLECTION_H
+#define DATAGRIDCOLLECTION_H
+
+#include <godot_cpp/classes/ref_counted.hpp>
+#include "data_grid.h"
+
+namespace godot {
+
+class DataGridCollection : public RefCounted {
+	GDCLASS(DataGridCollection, RefCounted)
+
+private:
+	Dictionary layered_datagrid_collection;
+
+protected:
+	static void _bind_methods();
+
+public:
+	DataGridCollection();
+	~DataGridCollection();
+
+	void clear() { layered_datagrid_collection.clear(); }
+	void add_datagrid_to_collection(const Point2i &p_datagrid_position, int p_layer, Ref<DataGrid> p_datagrid);
+	bool has_layer_at_pos(const Point2i &p_datagrid_position, int p_layer) const;
+	Ref<DataGrid> get_layer_at_pos(const Point2i &p_datagrid_position, int p_layer) const;
+	Dictionary filter_layers_at_pos(const Point2i &p_datagrid_coords, const Array &filter_layers) const;
+};
+}
+
+#endif // DATAGRIDCOLLECTION_H
