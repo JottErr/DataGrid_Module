bd46a05fdef4f18ab370091f591ab5d7b4932b5d
Jarik R <jarik.ruedebusch@web.de>
2023-09-29 15:19:27 +0200

    Update to use DataGridCompRef

--- demo/example/FollowMouse.gd
+++ demo/example/FollowMouse.gd
@@ -1,17 +1,17 @@
 extends Sprite2D
 
-
+@export var c_offset : Vector2
 @onready var data_grid_component: DataGridComponent = $DataGridComponent
 
 
 func _process(delta: float) -> void:
-	set_global_position(get_global_mouse_position())
+	set_global_position(get_global_mouse_position() + c_offset)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("scroll_down"):
-		data_grid_component.radius -= 15
-		print(data_grid_component.radius)
+		data_grid_component.set_radius(data_grid_component.get_radius() - 15)
+		print(data_grid_component.get_radius())
 	if Input.is_action_just_pressed("scroll_up"):
-		data_grid_component.radius += 15
-		print(data_grid_component.radius)
+		data_grid_component.set_radius(data_grid_component.get_radius() + 15)
+		print(data_grid_component.get_radius())
