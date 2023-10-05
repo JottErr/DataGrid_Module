42ecf6abb6c2cb07fe1088ae16bf7e7d4483b8fc
Jarik R <jarik.ruedebusch@web.de>
2023-10-05 03:48:10 +0200

    Add Player class to demo
    The Player class can be moved by keyboard or controller input.

---
+++ demo/example/Player/Player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+var speed := 220
+
+func _process(delta: float) -> void:
+	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = speed * dir
+	move_and_slide()
