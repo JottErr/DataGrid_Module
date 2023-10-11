faf6d8beca78c114dfab7d58f5a392b32d378ce8
Jarik R <jarik.ruedebusch@web.de>
2023-10-11 17:42:24 +0200

    Update demo

--- demo/example/Entity/IntelligenceController.gd
+++ demo/example/Entity/IntelligenceController.gd
@@ -34,12 +34,12 @@ func _on_think_cycle_timer_timeout() -> void:
 	var lin_curve := MathCurve.new()
 	lin_curve.set_parameters(-1, 1, 0, 1)
 	interest_grid.radiate_value_at_position(interest_grid.get_center(), 7, lin_curve, 1.0) #own pos
+	#combine threat of player and proximity of other entities, more weight to player threat
 	manager.add_into_datagrid_from_collection(interest_grid, 1, global_position, -2.0)
 	manager.add_into_datagrid_from_collection(interest_grid, 2, global_position, -1.0)
-	manager.add_into_datagrid_from_collection(interest_grid, 9, global_position, -3.0)
-
 	
-
+	
+	
 	#normalize layer mix and multiply by interest template to favor closer cells
 	interest_grid.normalize_data()
 	for y in interest_grid_size.y:
@@ -47,11 +47,17 @@ func _on_think_cycle_timer_timeout() -> void:
 			var value := interest_grid.get_cell_value(Vector2i(x, y)) * interest_template.get_cell_value(Vector2i(x, y))
 			interest_grid.set_cell_value(Vector2i(x, y), value)
 	
+	#set blocked cells (walls) to 0
+	manager.add_into_datagrid_from_collection(interest_grid, 9, global_position, -2.0)
+	#set cells out of boundaries to 0
+	manager.mark_cells_outside_boundaries(interest_grid, global_position, -2.0)
+
+
 	#Vector in cells from Entity to target, used to get a direction atm, could be made global_position
 	var target: Vector2i = interest_grid.get_highest_cell()
 	found_target.emit(Vector2(target - interest_grid.get_center()))
-	
-	
+
+
 	var corner_pos := manager.find_corner_from_center(global_position, interest_grid.get_center())
 	target_sprite.global_position = manager.snap_global_postion_to_cell_center(corner_pos + target * cell_size) 
 	grid_visualizer.global_position = corner_pos
