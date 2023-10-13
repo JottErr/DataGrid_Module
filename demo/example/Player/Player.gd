c6a8c23502b2f43b230932cc855c1e55ee0cb61b
Jarik R <jarik.ruedebusch@web.de>
2023-10-13 14:16:21 +0200

    Change player input actions and increase speed

--- demo/example/Player/Player.gd
+++ demo/example/Player/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-var speed := 220
+
+var speed := 250
+
 
 func _process(_delta: float) -> void:
-	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = speed * dir
 	move_and_slide()
