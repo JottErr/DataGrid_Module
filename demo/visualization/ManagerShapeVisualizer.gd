ab8ea04d69fe1e43aab0914f19d1564cc12890a8
Jarik R <jarik.ruedebusch@web.de>
2023-10-14 20:36:37 +0200

    Couple ManagerShapeVisualizer to DataGridManager
    Instead of having to set setting datagrid_count, datagrid_size and
    cell_size manually, ManagerShapeVisualizer now has a reference to a
    DataGridManager and gets the variables on `_draw`.

--- demo/visualization/ManagerShapeVisualizer.gd
+++ demo/visualization/ManagerShapeVisualizer.gd
@@ -2,28 +2,20 @@
 extends Node2D
 
 
-@export var datagrid_count: Vector2i : set = set_datagrid_count
-@export var datagrid_size: Vector2i : set = set_datagrid_size
-@export var cell_size: int : set = set_cell_size
+@export var manager : DataGridManager : set = set_manager
 
 
 func _draw():
+	var datagrid_count := manager.get_datagrid_count()
+	var datagrid_size := manager.get_datagrid_size()
+	var cell_size := manager.get_cell_size()
+	
 	for y in datagrid_count.y:
 		for x in datagrid_count.x:
 			var start := Vector2(0.5, 0.5) + Vector2(x, y) * Vector2(datagrid_size) * cell_size
 			draw_rect(Rect2(start, datagrid_size * cell_size), Color.BLACK, false, 1.0)
 
 
-func set_datagrid_count(value: Vector2i) -> void:
-	datagrid_count = value
-	queue_redraw()
-
-
-func set_datagrid_size(value: Vector2i) -> void:
-	datagrid_size = value
-	queue_redraw()
-
-
-func set_cell_size(value: int) -> void:
-	cell_size = value
+func set_manager(_manager: DataGridManager) -> void:
+	manager = _manager
 	queue_redraw()
